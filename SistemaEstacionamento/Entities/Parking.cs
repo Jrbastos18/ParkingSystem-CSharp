64c60eeb8210a3da85ebed941afe15c8dd436c5a
Zenalvo Junior <juniorrosalvo2015@gmail.com>
2024-01-07 00:57:49 -0300

    Challenge Parking system to practice object-oriented programming/Sistema de estacionamento para praticar programação orientada a objetos

---
+++ SistemaEstacionamento/Entities/Parking.cs
@@ -0,0 +1,73 @@
+﻿using System.Globalization;
+
+namespace SistemaEstacionamento.Entities
+{
+    internal class Parking
+    {
+        //Class properties
+        public double InitialPrice { get; private set; }
+        public double PricePerHour { get; private set; }
+        List<string> ListVehicle = new List<string>();
+
+        //Class constructor
+        public Parking(double initialPrice, double pricePerHour)
+        {
+            InitialPrice = initialPrice;
+            PricePerHour = pricePerHour;
+        }
+
+        //Class methods
+
+        //Method to add a vehicle
+        public void AddVehicle()
+        {
+            Console.Write("Enter the vehicle's license plate to park: ");
+            string vehicle = Console.ReadLine();
+
+            ListVehicle.Add(vehicle);
+        }
+
+        //Method to remove a vehicle
+        public void RemoveVehicle()
+        {
+            Console.Write("Enter the vehicle's license plate to remove: ");
+            string licensePlate = Console.ReadLine();
+
+            //Conditional structure to check if the vehicle exists
+            if (ListVehicle.Any(x => x.ToUpper() == licensePlate.ToUpper()))
+            {
+                Console.Write("Enter the number of hours the vehicle was parked: ");
+                int hoursParked = int.Parse(Console.ReadLine());
+
+                double totalValue = InitialPrice + PricePerHour * hoursParked;
+
+                ListVehicle.Remove(licensePlate);
+
+                Console.WriteLine($"The vehicle {licensePlate} was removed and the total price was: R${totalValue.ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that vehicle is not parked here. Check if you typed the license plate correctly");
+            }
+        }
+
+        //Method for listing vehicles
+        public void ListVehicles()
+        {
+            //Conditional structure to check if there are vehicles in the parking lot
+            if (ListVehicle.Any())
+            {
+                Console.WriteLine("The parked vehicles are:");
+                foreach (string vehicle in ListVehicle)
+                {
+                    Console.WriteLine(vehicle);
+                }
+            }
+            else
+            {
+                Console.WriteLine("There are no parked vehicles.");
+            }
+        }
+
+    }
+}
---
+++ SistemaEstacionamento/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Globalization;
+using SistemaEstacionamento.Entities;
+
+namespace SistemaEstacionamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the parking system");
+
+            Console.Write("Enter starting price: ");
+            double initialPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Now, enter the price per hour: ");
+            double pricePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Instantiates the parking class with the values obtained previously
+            Parking pa = new Parking(initialPrice, pricePerHour);
+
+            string option = string.Empty;
+            bool showMenu = true;
+
+            //Perform the menu loop while the showMenu is true
+            while (showMenu)
+            {
+                Console.Clear();
+                Console.WriteLine("Enter your option:");
+                Console.WriteLine("1 - Register vehicle");
+                Console.WriteLine("2 - Remove vehicle");
+                Console.WriteLine("3 - List vehicles");
+                Console.WriteLine("4 - Close");
+
+                switch(Console.ReadLine())
+                {
+                    case "1":
+                        pa.AddVehicle();
+                        break;
+                    case "2":
+                        pa.RemoveVehicle();
+                        break;
+                    case "3":
+                        pa.ListVehicles();
+                        break;
+                    case "4":
+                        showMenu = false; 
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option!");
+                        break;
+                }
+
+                Console.WriteLine("Press any key to continue");
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("The program ended!");
+        }
+    }
+}
